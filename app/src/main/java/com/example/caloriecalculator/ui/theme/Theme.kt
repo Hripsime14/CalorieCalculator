e62a5624b186e9def454bafed3cbd179413dfe1f
Hripsime14 <hripsime.ghazaryan.98@gmail.com>
2024-06-09 18:41:01 +0400

    Add UI.

--- app/src/main/java/com/example/caloriecalculator/ui/theme/Theme.kt
+++ app/src/main/java/com/example/caloriecalculator/ui/theme/Theme.kt
@@ -7,6 +7,13 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
+import com.example.core_ui.BrightGreen
+import com.example.core_ui.DarkGray
+import com.example.core_ui.DarkGreen
+import com.example.core_ui.LightGray
+import com.example.core_ui.MediumGray
+import com.example.core_ui.Orange
+import com.example.core_ui.TextWhite
 import com.example.core_ui.dimesion.Dimensions
 import com.example.core_ui.dimesion.LocalSpacing
 
